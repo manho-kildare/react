d5d6112f366193f2d7106ded9ad195057699905e
manho-kildare <10005@kildare.co.kr>
2023-04-14 15:55:29 +0900

    useState 훅 이해하기 시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import Callback from "./pages/Callback";
-import HightOrderCallback from "./pages/HightOrderCallback";
-import Memo from "./pages/Memo";
-import UserOrCreateTest from "./pages/UseOrCreateTest";
+
 
 function App() {
   return (
     <div>
-      <HightOrderCallback />
-      <Callback />
-      <Memo />
-      <UserOrCreateTest />
+      
     </div>
   );
 }

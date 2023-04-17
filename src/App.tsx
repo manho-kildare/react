b314c59f63d704ec49dbec9522582c84d5dae9e4
manho-kildare <10005@kildare.co.kr>
2023-04-17 13:38:05 +0900

    ch04_4 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import ArrayState from "./pages/ArrayState";
-import BasicForm from "./pages/BasicForm";
-import HigherOrderRadioInputTest from "./pages/HigherOrderRadioInputTest";
-import NumberState from "./pages/NumberState";
-import ObjectState from "./pages/ObjectState";
-import RadioInputTest from "./pages/RadioInputTest";
-import RadioInputTest2 from "./pages/RadioInputTest2";
-import ShowHideModal from "./pages/ShowHideModal";
+
 
 
 function App() {
   return (
     <main>
-      <ArrayState />
-      <ObjectState />
-      <BasicForm />
-      <HigherOrderRadioInputTest />
-      <RadioInputTest />
-      <RadioInputTest2 />
-      <ShowHideModal />
-      {/* <InputTest /> */}
-      <NumberState />
+      
     </main>
   );
 }
